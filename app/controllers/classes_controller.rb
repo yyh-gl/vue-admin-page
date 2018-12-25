8079a814bbc5a57dea0c13bbe8e0ff2c2ce95f8f
yyh-gl <yhonda.95.gl@gmail.com>
2018-12-26 03:36:40 +0900

    各講義ごとの学生出席状況を取得可能にした

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,5 +1,23 @@
 class ClassesController < ApplicationController
   def index
-    @class_list = send_request(ENV['REQ_CLASS_BY_PROFID'], {professor_id: "P-0050"})
+    @class_list = fetch_classes_by_prof_id('P-0050')
+
+    class_ids = @class_list.map { |data| data['classId']}
+    @student_ids = {}
+    class_ids.each do |class_id|
+      @student_ids[class_id] = fetch_classes_by_class_id(class_id)
+    end
   end
+
+  private
+
+  def fetch_classes_by_prof_id(prof_id)
+    response = send_request(ENV['REQ_CLASS_BY_PROF_ID'], { professor_id: prof_id })
+    response['classData']
+  end
+
+  def fetch_classes_by_class_id(class_id)
+    send_request(ENV['REQ_CLASS_BY_CLASS_ID'], { class_id: class_id })
+  end
+
 end
